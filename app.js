bf7da44bfdde0cf87d75be50b0bc06cb4c64876e
fachri-literasia <fachri.amin@literasia.co.id>
2021-11-17 22:41:23 +0700

    fix bug

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 var videoCallsRouter = require('./routes/videoCall');
-var chatsRouter = require('./routes/chat');
+// var chatsRouter = require('./routes/chat');
 
 var app = express();
 
@@ -16,9 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/video-call', videoCallsRouter);
-app.use('/chat', chatsRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/video-call', videoCallsRouter);
+// app.use('/chat', chatsRouter);
+app.use('/', videoCallsRouter);
 
 module.exports = app;
